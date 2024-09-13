f9160490c542713846609f5952cf652d3b2a13ce
Sujay Zope <sujayazope@gmail.com>
2024-09-13 12:30:31 +0530

    latest

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -1,5 +1,12 @@
-let i=1;
-while(i<=100){
-    console.log(i);
-    i++
-}
+// let i=1;
+// while(i<=100){
+//     console.log(i);
+//     i++
+// }
+
+// for-of loop
+
+let str = "ApnaCollege";
+for(let i of str){
+    console.log("i= ",i);
+};
