4a27e6c5b3e1d538b5ccbe55f9c0c7c91e564781
Sujay Zope <sujayazope@gmail.com>
2024-09-15 15:27:51 +0530

    update

--- JavaScript/loops.js
+++ JavaScript/loops.js
@@ -6,11 +6,32 @@
 
 // for-of loop
 
-let str = "ApnaCollege";
-let size=0;
-for(let i of str){
-    console.log("i= ",i);
-    size++;
-};
+// let str = "ApnaCollege";
+// let size=0;
+// for(let i of str){
+//     console.log("i= ",i);
+//     size++;
+// };
 
-console.log("Size of string = ", size);
+// console.log("Size of string = ", size);
+
+// for in loop
+
+// let obj = {
+//     name: "Sujay",
+//     age: 25,
+//     city: "Jalgaon",
+//     cgpa:7.74,
+//     isPassed: true,
+// };
+
+// for(let i in obj){
+//     console.log("i= ", i, "Value= ", obj[i]);
+// }
+
+// even no 0-100
+
+for(let i=0; i<=100;i++){
+    if(i%2==0)
+        console.log(i);
+}
