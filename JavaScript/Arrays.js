512e336aa4efa370c2bf10e396c3353684b59310
Sujay Zope <sujayazope@gmail.com>
2024-09-16 15:05:18 +0530

    update

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -1,20 +1,37 @@
-let arr=[21,45,64,32,49,87];
+// let arr=[21,45,64,32,49,87];
+// console.log(arr);
+
+// // Sorting the array in ascending order
+// arr.sort();
+// console.log(arr);
+
+// for(let i=0;i<arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+// let i=0;
+
+// for(i of arr ){
+//     console.log(i);
+// }
+
+// for(i in arr ){
+//     console.log(arr[i]);
+// }
+
+//avg marks of student
+
+// let marks=[87,90,75,60,85];
+// let sum=0;
+
+// for(let i=0;i<marks.length;i++){
+//     sum+=marks[i];
+// }
+// let avg= sum/marks.length;
+// console.log(avg);
+
+//splice
+
+let arr=[1,2,3,4,5,6,7,8,9];
+arr.splice(2,4,11,22,33);
 console.log(arr);
-
-// Sorting the array in ascending order
-arr.sort();
-console.log(arr);
-
-for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-}
-
-let i=0;
-
-for(i of arr ){
-    console.log(i);
-}
-
-for(i in arr ){
-    console.log(arr[i]);
-}
