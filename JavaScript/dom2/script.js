9c8ffbba441572fb08a6eb8e88f5ae7c29b212b3
Sujay Zope <sujayazope@gmail.com>
2024-09-19 22:42:07 +0530

    update

---
+++ JavaScript/dom2/script.js
@@ -0,0 +1,8 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Thank you for showing interest! Explore our services below.');
+});
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you shortly.');
+});
